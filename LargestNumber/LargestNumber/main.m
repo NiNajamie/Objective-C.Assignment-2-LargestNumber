917eff32c73a7600af720f70e2a1240c1403a0bf
NiNajamie <NiNajamie@users.noreply.github.com>
2016-04-23 23:20:48 -0700

    edited

--- LargestNumber/LargestNumber/main.m
+++ LargestNumber/LargestNumber/main.m
@@ -9,30 +9,42 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
+    
+    NSArray *num = @[@3,@7, @6, @8];
+    id max;
+//    [num objectAtIndex: i];
+    max = num[0];
+
+    for (int i = 1; i < [num count]; i++) {
+    
+        if (max < num[i]) {
+            max = num[i];
+        }
+    }
+    NSLog(@"The largest number is %@", max);
+    
+    NSArray *arr = @[@44,@5, @6];
+    id max2;
+    //    [num objectAtIndex: i];
+    max2 = arr[0];
+    
+    for (int i = 1; i < [arr count]; i++) {
         
-        NSArray *num = @[@3, @7, @6, @8];
-        num = [num sortedArrayUsingSelector:@selector(compare:)];
-        int max = [num[3] intValue];
-        NSLog(@"The largest number is %d", max);
-        
-        
-        NSArray *Num = @[@44, @5, @6];
-        Num = [Num sortedArrayUsingSelector:@selector(compare:)];
-        int Max = [[Num lastObject] intValue];
-        NSLog(@"The Largest number is %d", Max);
-        
-        
+        if (max2 < arr[i]) {
+            max2 = arr[i];
+        }
+    }
+    NSLog(@"The highest number is %@", max2);
+    return 0;
+}
+
 //        NSArray *a = @[@7.8, @2.1, @3.9, @5.0];
 //        a = [a sortedArrayUsingSelector:@selector(compare:)];
 //        double big = [[a lastObject] doubleValue];
 //        double small = [a[0] doubleValue];
 //        NSLog(@"The biggest number is %.1f", big);
 //        NSLog(@"the smallest number is %.1f", small);
-        
-    }
-    return 0;
-}
-
+    
+//
 // 3, 7, 6, 8
 // 44, 5, 6
